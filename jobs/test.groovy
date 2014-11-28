eaff82620947889e1eb30194345c81eae6396e06
Bussmeyer <thomas.bussmeyer@gmx.de>
2014-11-28 18:24:44 +0100

    branch creator

--- jobs/test.groovy
+++ jobs/test.groovy
@@ -1,1 +1,15 @@
-#
+def project = 'Bussmeyer/bussmeyer.github.io'
+def branchApi = new URL("https://api.github.com/repos/${project}/branches")
+def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
+branches.each {
+    def branchName = it.name
+    job {
+        name "${project}-${branchName}".replaceAll('/','-')
+        scm {
+            git("git://github.com/${project}.git", branchName)
+        }
+        steps {
+            #maven("test -Dproject.name=${project}/${branchName}")
+        }
+    }
+}
