c871c485fc7751599df9ebd3e959742419476ff1
Thomas Bußmeyer <thomas.bussmeyer@pixelpark.com>
2014-11-28 19:25:29 +0100

    #3 Add some comments.

--- jobs/build-package.groovy
+++ jobs/build-package.groovy
@@ -1,3 +1,5 @@
+// https://github.com/jenkinsci/job-dsl-plugin/wiki/Tutorial---Using-the-Jenkins-Job-DSL
+
 def project = 'Bussmeyer/bussmeyer.github.io'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
@@ -7,9 +9,20 @@ branches.each {
         name "PP Test - 1 Developer Jobs - Basic Build and Package - ${project} - ${branchName}".replaceAll('/','-')
         scm {
             git("git://github.com/${project}.git", branchName)
+            // Github Repo Browser
+            // Calculate changelog against a specific branch
+            // Wipe out repository & force clone
         }
         steps {
             //maven("test -Dproject.name=${project}/${branchName}")
+
+            // Build
+            // Package
+
+            // Post Build
+                // Activate Chuck Norris
+                // Artefakte archivieren
+                // Send artifacts over SSH?
         }
     }
 }
