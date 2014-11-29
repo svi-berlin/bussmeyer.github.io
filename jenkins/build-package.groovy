3901cd427c2b9118acf9926325a201f6316b1154
Thomas Bußmeyer <thomas.bussmeyer@pixelpark.com>
2014-11-29 12:56:01 +0100

    Split of fpm command.

--- jenkins/build-package.groovy
+++ jenkins/build-package.groovy
@@ -14,10 +14,11 @@ branches.each {
             // Wipe out repository & force clone
         }
         steps {
-            //maven("test -Dproject.name=${project}/${branchName}")
             // Build
-            def fpmCommand = "fpm -s dir -t rpm --name '${project}' --version 1 --iteration ${BUILD_NUMBER} --description 'Static Test Site' --maintainer 'thomas.bussmeyer@pixelpark.com' --vendor 'admin@pixelpark.com' --url 'http://www.pixelpark.com' --log info --verbose '${WORKSPACE}/src'".replaceAll('/','-')
-            shell(fpmCommand)
+            def fpmCommandBasics = "fpm -s dir -t rpm --name '${project}' --version 1 --iteration ${BUILD_NUMBER}".replaceAll('/','-')
+            def fpmCommandLogs = "--log info --verbose"
+            def fpmCommandProject = "--description 'Static Test Site' --maintainer 'thomas.bussmeyer@pixelpark.com' --vendor 'admin@pixelpark.com' --url 'http://www.pixelpark.com' '${WORKSPACE}/src'"
+            shell("${fpmCommandBasics} ${fpmCommandLogs} ${fpmCommandProject}")
             // Package
                 // Persist artefacts somewhere.
             // Post Build
